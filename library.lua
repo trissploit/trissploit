08f2570fb8a1fd9ebb4b38b95d597ba1d7f45b8f
trissploit <129907383+trissploit@users.noreply.github.com>
2025-12-30 16:11:53 -0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -1860,8 +1860,8 @@ do
         Library.Registry[WM.Label] = { TextColor3 = "FontColor", FontFace = "Font" }
         Library.Registry[WMStroke] = { Color = "OutlineColor" }
 
-        -- make watermark draggable (only when main UI is open)
-        Library:MakeDraggable(WM.Holder, WM.Holder, false)
+        -- make watermark draggable
+        Library:MakeDraggable(WM.Holder, WM.Holder, true)
 
         local lastTime = tick()
         local frameCount = 0
